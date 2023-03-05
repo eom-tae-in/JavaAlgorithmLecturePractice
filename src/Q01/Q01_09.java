02a05a57bf1d5c2334b6e000388579a512aa0cdb
eom-tae-in <https://github.com/eom-tae-in>
2023-03-05 23:49:36 +0900

    Feat: Q01_09

--- src/Q01/Q01_09.java
+++ src/Q01/Q01_09.java
@@ -1,2 +1,24 @@
-package Q01;public class Q01_09 {
+package Q01;
+
+import java.util.Scanner;
+
+public class Q01_09 {
+
+    public int solution(String str) {
+        int answer = 0;
+        char[] chars = str.toCharArray();
+        for (char c : chars) {
+            if (Character.isDigit(c)) {
+                answer = answer * 10 + c - '0';
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Q01_09 q01_09 = new Q01_09();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(q01_09.solution(str));
+    }
 }
