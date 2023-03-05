90e87980e63247221c5be5f29226dd187cf6f388
eom-tae-in <https://github.com/eom-tae-in>
2023-03-05 23:50:50 +0900

    Feat: Q01_12

--- src/Q01/Q01_12.java
+++ src/Q01/Q01_12.java
@@ -1,2 +1,25 @@
-package Q01;public class Q01_12 {
+package Q01;
+
+import java.util.Scanner;
+
+public class Q01_12 {
+
+    public String solution(String str, int n) {
+        String answer = "";
+        for (int i = 0; i < n; i++) {
+            String tmp = str.substring(0, 7).replace('#', '1').replace('*', '0');
+            int num = Integer.parseInt(tmp, 2);
+            answer += (char) num;
+            str = str.substring(7);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Q01_12 q01_12 = new Q01_12();
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        String str = sc.next();
+        System.out.println(q01_12.solution(str, number));
+    }
 }
