afd5313571f26e8e0cc71071a3061795e1ea321e
eom-tae-in <https://github.com/eom-tae-in>
2023-03-05 23:46:42 +0900

    Feat: Q01_06

--- src/Q01/Q01_06.java
+++ src/Q01/Q01_06.java
@@ -1,2 +1,21 @@
-package Q01;public class Q01_06 {
+package Q01;
+
+import java.util.Scanner;
+
+public class Q01_06 {
+    public String solution(String str) {
+        String answer = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.indexOf(str.charAt(i)) == i) {
+                answer += str.charAt(i);
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Q01_06 q01_06 = new Q01_06();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(q01_06.solution(str));
+    }
 }
