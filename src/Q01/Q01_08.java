1239c2fbfe8518ab1f5010c81d16f309ddba2195
eom-tae-in <https://github.com/eom-tae-in>
2023-03-05 23:49:15 +0900

    Feat: Q01_08

--- src/Q01/Q01_08.java
+++ src/Q01/Q01_08.java
@@ -1,2 +1,25 @@
-package Q01;public class Q01_08 {
+package Q01;
+
+import java.util.Scanner;
+
+public class Q01_08 {
+
+    public String solution(String str) {
+        String answer = "NO";
+        str = str.toUpperCase().replaceAll("[^A-Z]", "");
+        // 정규식에서 ^은 !와 같음 -> [^A-Z] (A~Z가 아닌 것들은 다 ""(빈문자열)로 바꿔라)
+        StringBuilder sb = new StringBuilder();
+        sb.append(str).reverse();
+        if (str.equals(String.valueOf(sb))) {
+            return "YES";
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Q01_08 q01_08 = new Q01_08();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(q01_08.solution(str));
+    }
 }
