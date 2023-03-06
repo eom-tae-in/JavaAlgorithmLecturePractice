d1b077518501fad813c70c48a3d2766f08ee6630
eom-tae-in <eti0728@daum.net>
2023-03-06 11:32:50 +0900

    Feat: Q02_02

--- src/Q02/Q02_02.java
+++ src/Q02/Q02_02.java
@@ -1,2 +1,29 @@
-package Q02;public class Q02_02 {
+package Q02;
+
+import java.util.Scanner;
+
+public class Q02_02 {
+
+    public int solution(int[] students) {
+        int answer = 1;
+        int maxValue = students[0];
+        for (int i = 1; i < students.length; i++) {
+            if (students[i] > maxValue) {
+                answer++;
+                maxValue = students[i];
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Q02_02 q02_02 = new Q02_02();
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int[] students = new int[number];
+        for (int i = 0; i < students.length; i++) {
+            students[i] = sc.nextInt();
+        }
+        System.out.println(q02_02.solution(students));
+    }
 }
