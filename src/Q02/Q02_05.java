439dd65443cb0ebcdfaf2dfc07961dccf841de78
eom-tae-in <eti0728@daum.net>
2023-03-06 11:34:59 +0900

    Feat: Q02_05

--- src/Q02/Q02_05.java
+++ src/Q02/Q02_05.java
@@ -1,2 +1,27 @@
-package Q02;public class Q02_05 {
+package Q02;
+
+import java.util.Scanner;
+
+public class Q02_05 {
+
+    public int eratosthenes(int number) {
+        int answer = 0;
+        int[] array = new int[number + 1];
+        for (int i = 2; i <= number; i++) {
+            if (array[i] == 0) {
+                answer++;
+            }
+            for (int j = i; j <= number; j += i) {
+                array[j] = 1;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Q02_05 q02_05 = new Q02_05();
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.println(q02_05.eratosthenes(number));
+    }
 }
