1c00d4d3a3e19357bcef48e4b6a5893163aff71a
eom-tae-in <eti0728@daum.net>
2023-03-06 11:35:23 +0900

    Faet: Q02_06

--- src/Q02/Q02_06.java
+++ src/Q02/Q02_06.java
@@ -1,2 +1,54 @@
-package Q02;public class Q02_06 {
+package Q02;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Q02_06 {
+
+    public ArrayList<Integer> solution(int[] array) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i : array) {
+            int num = flipNumber(i);
+            if (isPrime(num)) {
+                list.add(num);
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        Q02_06 q02_06 = new Q02_06();
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int[] array = new int[number];
+        for (int i = 0; i < number; i++) {
+            array[i] = sc.nextInt();
+        }
+        for (int i : q02_06.solution(array)) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public int flipNumber(int number) {
+        int tmp = number;
+        int res = 0;
+        while (tmp > 0) {
+            int t = tmp % 10;
+            res = res * 10 + t;
+            tmp /= 10;
+        }
+        return res;
+    }
+
+    public boolean isPrime(int number) {
+        if (number == 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
