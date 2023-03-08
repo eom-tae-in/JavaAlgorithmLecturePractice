91bb8506d5173e21bc46c39e8dd7519fc24e2ee2
eom-tae-in <eti0728@daum.net>
2023-03-08 10:58:11 +0900

    Feat: Q03_02

--- src/Q03/Q03_02.java
+++ src/Q03/Q03_02.java
@@ -1,2 +1,45 @@
-package Q03;public class Q03_02 {
+package Q03;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q03_02 {
+
+    public ArrayList<Integer> solution(int[] arr1, int[] arr2) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        int p1 = 0, p2 = 0;
+        while (p1 < arr1.length && p2 < arr2.length) {
+            if (arr1[p1] == arr2[p2]) {
+                answer.add(arr1[p1++]);
+                p2++;
+            }
+            else if (arr1[p1] < arr2[p2]) {
+                p1++;
+            } else {
+                p2++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Q03_02 q03_02 = new Q03_02();
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int[] arr1 = new int[num1];
+        for (int i = 0; i < num1; i++) {
+            arr1[i] = sc.nextInt();
+        }
+        int num2 = sc.nextInt();
+        int[] arr2 = new int[num2];
+        for (int i = 0; i < num2; i++) {
+            arr2[i] = sc.nextInt();
+        }
+        for (int i : q03_02.solution(arr1, arr2)) {
+            System.out.print(i + " ");
+        }
+    }
 }
