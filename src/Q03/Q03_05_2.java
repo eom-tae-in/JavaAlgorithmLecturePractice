1059b8ccf5991b236bf9bfd508183cbdeaa14a50
eom-tae-in <eti0728@daum.net>
2023-03-08 11:49:30 +0900

    Feat: Q03_05 Solution2

--- src/Q03/Q03_05_2.java
+++ src/Q03/Q03_05_2.java
@@ -1,2 +1,25 @@
-package Q03;public class Q03_05_2 {
+package Q03;
+
+import java.util.Scanner;
+
+public class Q03_05_2 {
+
+    public int solution(int num) {
+        int answer = 0, cnt = 1;
+        num = num - cnt;
+        while (num > 0) {
+            cnt++;
+            num = num - cnt;
+            if (num % cnt == 0) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Q03_05_2 q03_05 = new Q03_05_2();
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(q03_05.solution(num));
+    }
 }
