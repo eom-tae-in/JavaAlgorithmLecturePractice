b247a255d8fba49520137fa3c227a35413fa8664
eom-tae-in <eti0728@daum.net>
2023-03-10 10:41:07 +0900

    Feat: Q03_A01

---
+++ src/StackAndQueue/Q03_A01.java
@@ -0,0 +1,46 @@
+package StackAndQueue;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Q03_A01 {
+
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int pos : moves) {
+            for (int i = 0; i < board.length; i++) {
+                if (board[i][pos - 1] != 0) {
+                    int tmp = board[i][pos - 1];
+                    board[i][pos - 1] = 0;
+                    if (!stack.isEmpty() && stack.peek() == tmp) {
+                        stack.pop();
+                        answer += 2;
+                    } else {
+                        stack.push(tmp);
+                    }
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Q03_A01 q03_A01 = new Q03_A01();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] board = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+        int m = sc.nextInt();
+        int[] moves = new int[m];
+        for (int i = 0; i < m; i++) {
+            moves[i] = sc.nextInt();
+        }
+        System.out.println(q03_A01.solution(board, moves));
+    }
+}
